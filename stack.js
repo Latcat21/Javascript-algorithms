781d500d42a810278d9a6e8a638ddf50df4268bb
Brian <Brian@Brians-MacBook-Air.local>
2022-09-20 18:35:34 -0500

    adding to stack done

--- stack.js
+++ stack.js
@@ -7,10 +7,14 @@ class Stack {
   // Add element to top of stack
   push(element){
     this.items[this.count] = element
+    console.log(`${element} added to ${this.count}`)
     this.count += 1
-    
+
     return this.count - 1
   }
 
 }
 const stack = new Stack()
+stack.push(100)
+stack.push(200)
+stack.push(300)
