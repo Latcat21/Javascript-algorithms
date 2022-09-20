cea5a6cbc84b48b0f8209a8c753c6104977dd5a7
Brian <Brian@Brians-MacBook-Air.local>
2022-09-20 18:53:15 -0500

    stack all done

--- stack.js
+++ stack.js
@@ -13,8 +13,54 @@ class Stack {
     return this.count - 1
   }
 
+  // return and remove top element in stack
+  // return undefined if stack is empty
+  pop(){
+    if(this.count == 0) return undefined
+    let deleteItem = this.items[this.count - 1]
+    this.count -= 1
+    console.log(`${deleteItem} removed`)
+    return deleteItem
+  }
+
+  // Check top element in stack
+  peek(){
+    console.log(`top element is ${this.items[this.count - 1]}`)
+    return this.items[this.count - 1]
+  }
+  
+  // check if stack is empty
+  isEmpty(){
+    console.log(this.count == 0 ? 'stack is empty' : 'stack is not empty')
+    return this.count == 0
+  }
+  
+  // Check size of stack
+  size(){
+    console.log(`${this.count} elements in stack`)
+    return this.count
+  }
+
+  //print elements in stack
+  print(){
+    let str = ''
+    for(let i = 0; i < this.count; i++){
+      str += this.items[i] + ' '
+    }
+    return str
+  }
+
 }
 const stack = new Stack()
+
+stack.isEmpty()
 stack.push(100)
 stack.push(200)
 stack.push(300)
+
+stack.pop()
+stack.pop()
+console.log(stack.print())
+stack.peek()
+stack.isEmpty()
+stack.size()
